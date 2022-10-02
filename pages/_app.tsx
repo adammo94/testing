436f539b7d0be9dcf4a50a457fdbe3953bad300c
Thomas <cyzmenpl@gmail.com>
2022-10-02 17:18:49 +0200

    changed wrapper in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { wrapper } from "../store/store";
+import React from 'react';
+import {Provider} from 'react-redux';
+import {AppProps} from 'next/app';
+import {wrapper} from '../store/store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({Component, ...rest}: AppProps) => {
+    const {store, props} = wrapper.useWrappedStore(rest);
+    return (
+        <Provider store={store}>
+            <Component {...props.pageProps} />
+        </Provider>
+    );
+};
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
