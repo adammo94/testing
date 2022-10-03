8d3f439e98e86beef9eb88e5e81d35c7535751e7
Thomas <cyzmenpl@gmail.com>
2022-10-03 17:18:29 +0200

    eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import { Button } from "@mui/material"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { Button } from '@mui/material';
+import {
+  signIn, signOut, useSession,
+} from 'next-auth/react';
 
 export default function Component() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   return (
-    <>
-      <Button onClick={() => session ? signOut() : signIn()}>
-        {session ? 'Sign out' : 'Sign in'}
-      </Button>
-    </>
-  )
+    <Button onClick={() => (session ? signOut() : signIn())}>
+      {session ? 'Sign out' : 'Sign in'}
+    </Button>
+  );
 }
