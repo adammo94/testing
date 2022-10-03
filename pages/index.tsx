dca956aa855a93b352b311fd702adc880698228f
adammo94 <adammo666@gmail.com>
2022-10-03 19:17:47 +0200

    final update

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Component() {
 
   return (
     <Button onClick={() => (session ? signOut() : signIn())}>
-      c
+      d
       {session ? 'Sign out' : 'Sign in'}
     </Button>
   );
