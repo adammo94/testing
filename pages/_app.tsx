0513d36cd59ad83557c505248fcf6bbdb8ffba85
ThomasFront <cyzmenpl@gmail.com>
2022-10-05 12:23:02 +0200

    created global styles from styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app';
 import { wrapper } from '../store/store';
 import { Session } from 'next-auth';
 import { SessionProvider } from "next-auth/react"
+import GlobalStyle from 'globalstyles/globalstyles';
 
 const MyApp = ({
   Component,
@@ -13,6 +14,7 @@ const MyApp = ({
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
+        <GlobalStyle/>
         <Component {...rest} />
       </Provider>
     </SessionProvider>
