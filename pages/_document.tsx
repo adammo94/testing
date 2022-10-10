adca7d8c8a3807c6aed3b04ed077c7c3f50d1e13
Thomas <cyzmenpl@gmail.com>
2022-10-10 18:16:57 +0200

    changed _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,6 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap"
