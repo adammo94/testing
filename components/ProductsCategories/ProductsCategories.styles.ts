53b476025f223a7c20a568665ab7541090185c5f
adammo94 <adammo666@gmail.com>
2022-10-12 21:41:49 +0200

    updated categories component added products component, added toggle for categories component

--- components/ProductsCategories/ProductsCategories.styles.ts
+++ components/ProductsCategories/ProductsCategories.styles.ts
@@ -2,9 +2,20 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   position: absolute;
-  top: 64px;
+  top: 56px;
   left: 0;
   display: flex;
-  height: 100%;
-  background-color: red;
+  height: calc(100% - 56px);
+  width: 200px;
+  box-shadow: 5px 5px 5px rgba(0,0,0,0.05);
+  background-color: white;
+
+  ul {
+    width: 100%;
+  }
+
+  @media (min-width: 600px) {
+    height: calc(100% - 64px);
+    top: 64px;
+  }
 `;
