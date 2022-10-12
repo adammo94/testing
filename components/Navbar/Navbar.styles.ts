bedf2d35ca2f1740c6c3d182ca07a5ed233941e4
ThomasFront <cyzmenpl@gmail.com>
2022-10-12 10:37:59 +0200

    changed navbar design and transferred sign in button

---
+++ components/Navbar/Navbar.styles.ts
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material';
+import styled from 'styled-components';
+
+export const SignInButton = styled(Button)`
+  background-color: white;
+  color: black;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
-import { Button } from '@mui/material';
-import {
-  useSession,
-} from 'next-auth/react';
-import { useRouter } from 'next/router';
-
 export default function Component() {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  if (session) {
-    return <> </>;
-  }
-
   return (
-    <Button
-      variant="contained"
-      onClick={() => (router.push('/login'))}
-    >
-      Sign in
-    </Button>
+    <> </>
   );
 }
